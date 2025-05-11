2fbd3f67324d2d44f6875feb02a4dbe8baa7538c
teklu abayneh <tekluabayneh@gmail.com>
2025-05-11 21:30:25 +0300

    Updated src/script.ts

--- src/script.ts
+++ src/script.ts
@@ -1,18 +1,34 @@
 import Showdown from "showdown";
-
+import sharejs from "share";
 window.onload = () => {
   let converter = new Showdown.Converter();
   let pad = document.getElementById("pad")! as HTMLTextAreaElement;
   let MarkdownArea = document.getElementById("markdown")! as HTMLElement;
-
+  let previousMarkDownText: string | number;
   let ConvertTextAreaToMarkdown = () => {
     let markdownText = pad.value;
+    previousMarkDownText = markdownText;
     let html = converter.makeHtml(markdownText);
     MarkdownArea.innerHTML = html;
     console.log(pad.value);
   };
 
-  pad.addEventListener("input", ConvertTextAreaToMarkdown);
+  let DidChangeOccur = (): boolean => {
+    if (previousMarkDownText != pad.value) {
+      return true;
+    } else return false;
+  };
 
-  ConvertTextAreaToMarkdown();
+  setInterval(() => {
+    if (DidChangeOccur()) {
+      ConvertTextAreaToMarkdown();
+    }
+  }, 1000);
+
+  sharejs.open("home", "text", (error: Error, doc: any) => {
+    doc.attach_textArea(pad);
+    ConvertTextAreaToMarkdown();
+  });
+
+  pad.addEventListener("input", ConvertTextAreaToMarkdown);
 };
